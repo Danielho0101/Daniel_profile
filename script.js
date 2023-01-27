b925162645e23315c064e823bf7f1675ac8f8879
Danielho0101 <lsps08@yahoo.com.hk>
2023-01-27 22:58:55 +0800

    fixed photo and download My CV function

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 function display_alert(){
     alert("Hello, welcome to my website!")
 };
+
+
+// need to fix the loading problem 
+
+let get_name = window.prompt("What is you name?");
+
+window.alert("Wellcome to my website " + get_name);
+
